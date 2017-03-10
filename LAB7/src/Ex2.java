2962463bd49dec5e05df3b51b0a85ff966d437ae
Luke Creighton <lk479778@dal.ca>
2017-03-10 09:59:28 -0400

    //Ex1 DONE //Ex2 STARTED

---
+++ LAB7/src/Ex2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ex2 {
+	public static void main(String[] args){
+		Scanner kb = new Scanner(System.in);
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+			
+	}
+}
